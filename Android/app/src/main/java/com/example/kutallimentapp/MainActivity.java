38f72d72125e759d29c93ed7c8fd0267b8ad7067
Adrian Estevez <42591423+estevezadrian95@users.noreply.github.com>
2022-11-07 16:47:05 -0300

    Configuracion inicial sin MVP
    Primer avance con el acelerometro. Resta implementarlo con MVP

--- Android/app/src/main/java/com/example/kutallimentapp/MainActivity.java
+++ Android/app/src/main/java/com/example/kutallimentapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.kutallimentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +19,11 @@ public class MainActivity extends AppCompatActivity implements ContractMain.Main
 
     ContractMain.PresenterMainActivity presenter;
 
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+    int whip = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +33,57 @@ public class MainActivity extends AppCompatActivity implements ContractMain.Main
         botonServirComida = findViewById(R.id.botonServirComida);
         presenter = new PresenterMainActivity(this, new ArduinoModel());
 
-        botonServirComida.setOnClickListener(new View.OnClickListener(){
+        botonServirComida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 presenter.onButtonClick();
             }
         });
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (sensor == null) {
+            finish();
+        }
+
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float x = sensorEvent.values[0];
+                System.out.println("Valor de giro" + x);
+                //movemos hacia la derecha
+                if (x < -5 && whip == 0) {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                    whip++;
+                }
+                //movemos hacia la izquierda
+                else if (x > 5 && whip == 1) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                    whip++;
+                }
+
+                if (whip == 2) {
+                    whip = 0;
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        start();
     }
+
+    private void start() {
+        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void stop() {
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
 
     @Override
     protected void onDestroy() {
@@ -38,7 +92,13 @@ public class MainActivity extends AppCompatActivity implements ContractMain.Main
     }
 
     @Override
-    public void setString(String string){
+    public void setString(String string) {
         mensajeArduino.setText(string);
+    }
+
+    @Override
+    protected void onPause() {
+        stop();
+        super.onPause();
     }
 }
